146ba58e14d2766f4b9e7121fbbdcd5a6dcd3113
Matvey <mozhevm@gmail.com>
2025-10-01 21:18:06 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import {WithAuthHoc} from '@/lib/hocs/auth-hoc';
 import React from 'react';
 
 const Page = () => {
     return (
-        <h1>
-            Hello, world!
-        </h1>
+        <WithAuthHoc>
+            <h1>
+                content page
+            </h1>
+        </WithAuthHoc>
     );
 };
 
